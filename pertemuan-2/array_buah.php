ccb9db80304ee50dca073fde8002100218646e99
Varrel1608 <Varrelnf@gmail.com>
2025-02-28 16:11:28 +0700

    praktikum pert.2

---
+++ pertemuan-2/array_buah.php
@@ -0,0 +1,37 @@
+<?php
+    $ar_buah = ["Pepaya", "Mangga", "Pisang", "Jambu"];
+    //menampilkan buah index ke 1
+    echo $ar_buah[1];
+    //menampilkan jumlah buah 
+    echo "<br/>Jumlah Buah " . count($ar_buah);
+
+    //menampilkan seluruh buah 
+    echo "<ol>";
+    foreach ($ar_buah as $buah) {
+        echo "<li>" . $buah ."</li>";
+    } echo "</ol>";
+    
+    //menambahkan buah 
+    $ar_buah[] = "Apel";
+
+    //menghapus buah index ke 1
+    unset ($ar_buah[1]);
+
+    //mengubah index ke 2 menjadi durian
+    $ar_buah[1] = "Durian";
+
+    //menampilkan buah index ke 2
+    echo "<ul>";
+    foreach ($ar_buah as $Buah => $v) {
+        echo "<li>Buah index - " .$Buah . " adalah " .$v. "</li>";
+    }
+    echo "</ul>";
+
+    echo "<ul>";
+    sort($ar_buah);
+    foreach ($ar_buah as $Buah => $v) {
+        echo "<li>Buah index - " .$Buah . " adalah " .$v. "</li>";
+    }
+    echo "</ul>";
+    
+?>
---
+++ pertemuan-2/variable_user.php
@@ -0,0 +1,10 @@
+<?php
+    // Mendifinisikan variables
+    $nama = "Wahyu Ahmad Yassin";
+    $umur = 21;
+    $bb = 93;
+
+    echo "Nama saya adalah : " .$nama;
+    echo "<br/>Umur :" .$umur. "Tahun";
+    echo "<br/>Berat Badan : " .$bb. "Kg";
+?>
